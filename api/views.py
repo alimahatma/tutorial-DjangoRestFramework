613db0ef0cd69d7c409dfaafb0be0282e1587a6b
ali <laluallistilo@gmail.com>
2023-05-18 16:49:37 +0800

    add Dell and Update for RestApi

--- api/views.py
+++ api/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import action
 from .models import Movie, Rating
 from .serializer import MovieSerializer, RatingSerializer
+from django.contrib.auth.models import User
 
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
@@ -13,9 +14,27 @@ class MovieViewSet(viewsets.ModelViewSet):
     def rate_movie(self, request, pk=None):
         if 'stars' in request.data:
             movie=Movie.objects.get(id=pk)
-            print("Movie Title", movie.title)
-            response = {'message':'its working'}
-            return Response(response, status=status.HTTP_200_OK)
+            stars=request.data['stars']
+            # user=request.user
+            user=User.objects.get(id=id)
+            # print("User", user.username)
+
+            #update method on Django RestApi
+            try:
+                rating=Rating.objects.get(user=user.id, movie=movie.id)
+                rating.stars=stars
+                rating.save()
+
+                #add rating for serializers
+                serializer=RatingSerializer(rating, many=False)
+                response = {'message':'Rating is Update..','result':serializer.data}
+                return Response(response, status=status.HTTP_200_OK)
+            except:
+                rating=Rating.objects.create(user=user, movie=movie, stars=stars)
+                serializer=RatingSerializer(rating, many=False)
+                response={'message':'Rating is Created ', 'result':serializer.data}
+                return Response(response, status=status.HTTP_200_OK)
+            
         else:
             response = {'message':'You need to provide stars'}
             return Response(response, status=status.HTTP_400_BAD_REQUEST)
