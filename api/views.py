b29ddde5854df2124518584bcd2fb2113b9458bd
ali <laluallistilo@gmail.com>
2023-05-18 11:49:35 +0800

    custom method to restAPI with package docorator action for the status confirm

--- api/views.py
+++ api/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from rest_framework import viewsets
+# from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from .models import Movie, Rating
 from .serializer import MovieSerializer, RatingSerializer
 
@@ -7,7 +9,14 @@ class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
 
+    @action(detail=True, methods=['POST'])
+    def rate_movie(self, request, pk=None):
+        response = {'message':'its working'}
+        return Response(response, status=status.HTTP_200_OK)
+
 class RatingViewSet(viewsets.ModelViewSet):
     queryset = Rating.objects.all()
     serializer_class = RatingSerializer
+
+
 
